04e4b971b2497b981b6afc765b2bf3443e64a66f
Your Name <rajatpandey542@gmail.com>
2026-04-12 23:04:35 +0530

    First code in PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+echo "Hello World <br>";
+echo "Hello World <br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button onclick="run()">Order a Pizza</button>
+</body>
+</html>
+
+<script>
+    function run()
+    {
+        console.log("Im running")
+    }
+</script>
